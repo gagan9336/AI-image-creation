de197df020d6c865cc2547efd4f3337eb232df58
gagan9336 <deepgagan9336@gmail.com>
2023-02-01 13:12:05 +0530

    logo change

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
+import logoImagy from './logo.png';
 
 export {
   download,
   logo,
   preview,
+  logoImagy
 };
